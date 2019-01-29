dadf6856406f179ac2331a7b09ea021cadbe8d2d
Baron Desmond <baron@desmond.com>
2019-01-29 15:19:45 -0600

    t

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -3,6 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <p><form method=POST action=<?php echo base_url(); ?>login/auth/></p>
 <p>Name <input type=text name=EmpName> 
-<p><input type=text name=Email></p>
+<p>Email <input type=text name=Email></p>
 <p><input type=submit name=Submit></p>
 
