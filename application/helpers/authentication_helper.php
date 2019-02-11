5439be5f10f33b80b3cd97a7e3eb280a3432cbbf
Baron Desmond <baron@desmond.com>
2019-02-11 13:06:18 -0600

    t

--- application/helpers/authentication_helper.php
+++ application/helpers/authentication_helper.php
@@ -67,6 +67,8 @@
 		if (1)
 		{
 		//echo '<p>Job: ' . $event->Name . ' Dispatch: ' . $event->Dispatch . ' Start: ' . $event->StartDate . ' StopDate: ' . $event->StopDate . ' event: ' .$event->event . '</p>';
+		echo ${$eva};
+		exit;
 		if (!isset($Time[$event->EmpNo][${$eva}]) )
 		{
 			$Time[$event->EmpNo][${$eva}] = '';
