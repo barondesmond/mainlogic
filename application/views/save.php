771bd90de38285dd0b412958dc651f0785d88257
Baron Desmond <baron@desmond.com>
2019-02-15 08:12:47 -0600

    t

--- application/views/save.php
+++ application/views/save.php
@@ -2,6 +2,19 @@
 	$db = timeclock_employee($TimeClock);
 	$Job = $db['Job'];
 	$Save = $db['Save'];
+
+$Employee['EmpNo'] = '<option value="/review/index/">Select Employee</option>';
+$Employee = array_merge($Employee, $db['Employee']);
+
+//Employee Select
+echo 'Employee <select name="EmpNo" onchange="javascript:location.href = this.value;">';
+
+foreach ($Employee as $Emp)
+{
+	echo $Emp;
+}
+echo '</select>';
+echo "<BR><p>\r\n";
 if (isset($_REQUEST['EmpNo']))
 {
 	echo '<input type=hidden name="EmpNo" value="' . $_REQUEST['EmpNo'] . '">';
--- application/views/save.php.bak
+++ application/views/save.php.bak
@@ -1,5 +1,19 @@
 <?php 
 	$db = timeclock_employee($TimeClock);
-	var_dump($db['Save']);
-
+	$Job = $db['Job'];
+	$Save = $db['Save'];
+if (isset($_REQUEST['EmpNo']))
+{
+	echo '<input type=hidden name="EmpNo" value="' . $_REQUEST['EmpNo'] . '">';
+	if (isset($Job[$_REQUEST['EmpNo']]))
+	{
+		foreach ($Job[$_REQUEST['EmpNo']] as $Name => $LocName)
+		{
+			echo '<p>' . $LocName . "<BR>\r\n";
+			echo "Adjusted<BR>\r\n";
+			echo $Save[$_REQUEST['EmpNo']][$Name];
+			echo '</p>';
+		}
+	}
+}
 ?>
