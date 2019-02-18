b67c8610dc404b83e62cf72f45d33f6e5c0c8543
Baron Desmond <baron@desmond.com>
2019-02-18 16:33:07 -0600

    t

--- application/views/timesheet.php
+++ application/views/timesheet.php
@@ -1,1 +1,24 @@
 TimeSheet and TimeClock Together Again
+
+<?php
+	$db = timeclock_employee($TimeClock);
+	$Job = $db['Job'];
+	$Save = $db['Save'];
+
+$Employee['EmpNo'] = '<option value="/review/index/">Select Employee</option>';
+$Employee = array_merge($Employee, $db['Employee']);
+
+//Employee Select
+echo 'Employee <select name="EmpNo" onchange="javascript:location.href = this.value;">';
+
+foreach ($Employee as $Emp)
+{
+	echo $Emp;
+}
+echo '</select>';
+echo "<BR><p>\r\n";
+if (isset($_REQUEST['EmpNo']))
+{
+
+
+}
