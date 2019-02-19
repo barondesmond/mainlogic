19a2eb5ec85ae3f2473cf382c5912125cbc637ea
Baron Desmond <baron@desmond.com>
2019-02-19 09:11:51 -0600

    t

--- application/views/timesheet.php
+++ application/views/timesheet.php
@@ -20,7 +20,7 @@ if (isset($_REQUEST['EmpNo']))
 {
 	echo '<form method=post action=/review/timepost/>';
 	echo '<table>';
-	echo '	<tr>';
+	echo '	<tr border=1>';
 	echo ' <td>Wage Item</td><td>Job</td><td>JobClass</td>';
 	echo '<td>Date1-?</td>';
 	echo '<td>Total</td><td>Department</td><td>Dispatch</td><td>Memo</td><td>WorkmansCompCode</td><td>Processed</td>';
