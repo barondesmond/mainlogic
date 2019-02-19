f70f399621b89cacbede39433134635d9dc1da0a
Baron Desmond <baron@desmond.com>
2019-02-19 09:25:12 -0600

    t

--- application/views/timesheet.php
+++ application/views/timesheet.php
@@ -1,6 +1,33 @@
 TimeSheet and TimeClock Together Again
 <p>
 <?php
+function hour_head()
+{
+		echo '	<tr>';
+	echo '<td>Employee</td>';
+	echo ' <td>Wage Item</td><td>Job</td><td>JobClass</td>';
+	echo '<td>Date</td>';
+	echo '<td>Hours</td><td>Department</td><td>Dispatch</td><td>Memo</td><td>WorkmansCompCode</td><td>Processed</td>';
+	echo '</tr>';
+}
+
+function hour_row($db)
+{
+	$array = array('EmpNo', 'PRPayItem', 'JobID', 'JobClassId', 'Date', 'Hours', 'DeptID', 'Dispatch' 'Desc', 'WorkcompID', 'Processed');
+	echo '<tr>';
+
+	foreach ($array as $key)
+	{
+		echo '<td>';
+		if (isset($db[$key]))
+		{
+			echo $db[$key];
+		}
+		echo '</td>';
+	}
+	echo '</tr>';
+}
+
 $db = timesheet_employee($TimeSheet);
 $Employee['EmpNo'] = '<option value="/review/index/">Select Employee</option>';
 $Employee = array_merge($Employee, $db['Employee']);
@@ -20,11 +47,11 @@ if (isset($_REQUEST['EmpNo']))
 {
 	echo '<form method=post action=/review/timepost/>';
 	echo '<table border=1>';
-	echo '	<tr>';
-	echo ' <td>Wage Item</td><td>Job</td><td>JobClass</td>';
-	echo '<td>Date1-?</td>';
-	echo '<td>Total</td><td>Department</td><td>Dispatch</td><td>Memo</td><td>WorkmansCompCode</td><td>Processed</td>';
-
-	echo '</tr></table>';
+	hour_head();
+	foreach ($Timesheet[$_REQUEST['EmpNo']] as $row)
+	{
+		hour_row($row);
+	}
+	echo '</table>';
 
 }
