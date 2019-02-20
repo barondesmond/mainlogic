0bfa81cedf73e2c6cbb99ee0327075e076753e66
Baron Desmond <baron@desmond.com>
2019-02-20 11:12:04 -0600

    t

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,7 +1,7 @@
 		</div>
 </div>
 
-		<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+		<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. __DEV__</p>
 	
 </body>
 </html>
---
+++ application/views/footer.php.bak
@@ -0,0 +1,7 @@
+		</div>
+</div>
+
+		<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	
+</body>
+</html>
