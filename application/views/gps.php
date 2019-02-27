7fd9d4b5417cbebdd8223f9069dd8f2dfa5c8d36
Baron Desmond <baron@desmond.com>
2019-02-27 12:52:41 -0600

    t

--- application/views/gps.php
+++ application/views/gps.php
@@ -29,16 +29,19 @@ function table_form($row)
 	$table = '<td><form method=post action=/gps/update/><input type=hidden name=file value="' . $row->file . '"><input type=submit name="Accept" value="Accept"><input type=submit name="Deny" value="Deny"></form>';
 return $table;
 }
-foreach($location as $LocName=>$row)
+foreach($location as $lid=>$lc)
 {
-	if (!isset($tablehead))
+	foreach (${$lid} as $id=>$row)
 	{
-		$tablehead = table_head($row);
-		$table = '<table border=1><tr>' . $tablehead . '<td>Accept/Deny Override</td></tr>';
+		if (!isset($tablehead))
+		{
+			$tablehead = table_head($row);
+			$table = '<table border=1><tr>' . $tablehead . '<td>Accept/Deny Override</td></tr>';
+		}
+		$heablerow = table_row($row) . table_form($row);
+		$table .= '<tr>' . $tablerow . '</tr>';
 	}
 
-	$heablerow = table_row($row) . table_form($row);
-	$table .= '<tr>' . $tablerow . '</tr>';
 	
 }
 $table .= '</table>';
