be71998283f58353238ea69ea3091230ebe697da
Baron Desmond <baron@desmond.com>
2019-02-27 13:16:41 -0600

    t

--- application/views/gps.php
+++ application/views/gps.php
@@ -11,7 +11,7 @@ function table_row($row)
 	{
 		if ($key == 'file')
 		{
-			$value = '<img src="' . APPURL . 'upload/' . str_replace(' ', '%20', $value) . '" width=100%>';
+			$value = '<img src="' . APPURL . 'upload/' . str_replace(' ', '%20', $value) . '" width=10%>';
 		}
 		$table .= '<td>' . $value . '</td>';
 	}
