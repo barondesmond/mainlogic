0d2593b9977852679f08ee1e88e7ede1a7627976
Baron Desmond <baron@desmond.com>
2019-03-04 10:55:07 -0600

    t

---
+++ application/views/assign.php
@@ -0,0 +1,18 @@
+<form method=post action=/assignment/add/>
+
+Employee <select name=Employee>
+<option value=0195>Baron Desmond</option
+<option value=0002>Shannon Dillon</option>
+</select
+
+Group <select name=Group>
+<option>Group1</option>
+<option>Group2</option>
+</select>
+
+Job <selecct name=Job>
+<option value=J-012345>Job 1</option>
+<option value=J-023423>Job 2</option>
+<input type=submit value="Assign"></form>
+
+
