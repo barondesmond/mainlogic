c596e775348cb4d4ac65c79b5f71d7263e790e8f
Baron Desmond <baron@desmond.com>
2019-03-21 15:31:25 -0500

    t

--- application/views/main.php
+++ application/views/main.php
@@ -2,19 +2,15 @@
 
 <link type="text/css" rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"/>
 
-<style>
-
-
-</style>
 
 
 
  </head>
  <body>
 
-  <?php echo $mainnav;?><!-- content -->
+  
+<?php echo $mainnav;?><!-- content -->
  <?php echo $topnav;  ?>  <p><?php echo $content;?></p>
-    </div>
    <?php echo $widget2; ?>  <!--HTML Widget code-->
   
 
