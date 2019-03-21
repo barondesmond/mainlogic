98ebae5ce25f4b1078fbb78d11298ac32c46f101
Baron Desmond <baron@desmond.com>
2019-03-21 13:42:15 -0500

    t

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,6 +1,4 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
-<div><? echo $period ; } ?></div>
+
+<div><? if (isset($period) { echo $period ; } ?></div>
 <form method=post action=/timesheet/><input type=submit class="buttonmain" value="UPDATE"></form>
-<div><? echo $input ; } ?></div>
+<div><? if (isset($input) { echo $input ; } ?></div>
