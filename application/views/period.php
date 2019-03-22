c3135c7a9720b4941db7a500bc3908d2531bd611
Baron Desmond <baron@desmond.com>
2019-03-22 14:04:23 -0500

    t

--- application/views/period.php
+++ application/views/period.php
@@ -1,9 +1,9 @@
 <?php
-print_r($TimeClock);
+
 if (isset($TimeClock) && isset($controller) && isset($func))
 {
 	echo 'controller = ' . $controller . ' func = ' . $func;
-
+print_r($TimeClock);
 	echo	period_check($TimeClock, $controller, $func);
 }
 ?>
