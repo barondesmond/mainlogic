dff3540615b200d3ff3ec27effbb780514f85b04
Baron Desmond <baron@desmond.com>
2019-03-22 14:13:56 -0500

    t

--- application/views/period.php
+++ application/views/period.php
@@ -3,7 +3,8 @@ echo 'controller ' . $controller . ' func ' . $func;
 if (isset($TimeClock) && isset($controller) && isset($func))
 {
 	echo 'controller = ' . $controller . ' func = ' . $func;
-
+	echo 'entering period check';
 	period_check($TimeClock, $controller, $func);
+	echo 'exiting period check';
 }
 ?>
