61b494c026586f3a2a848a5ad88236759ff15b10
Baron Desmond <baron@desmond.com>
2019-03-25 16:24:29 -0500

    t

--- application/views/input.php
+++ application/views/input.php
@@ -17,11 +17,14 @@ if (!isset($_REQUEST['EmpNo']) || $_REQUEST['EmpNo'] == '')
 		$time = $Stop-$Start;
 		$days = $time / 86400;
 		echo 'Date <select name=StartDay  onchange="javascript:location.href = this.value;"><OPTION value="">Select Date</OPTION>';
-		for ($i=0; $i < $days; $i++)
+		for ($i=0; $i < $days+1; $i++)
 		{
-			$StartDay = date("Y:m:d", $Start + $i*86400);
+			if ($Start+$i*86400 < time())
+			{
+				$StartDay = date("Y:m:d", $Start + $i*86400);
 
-			echo '<OPTION value="' . '?Offset=' . $_REQUEST['Offset'] . '&EmpNo=' . $_REQUEST['EmpNo'] . '&StartDay=' . $StartDay . '">' . $StartDay . '</OPTION>';
+				echo '<OPTION value="' . '?Offset=' . $_REQUEST['Offset'] . '&EmpNo=' . $_REQUEST['EmpNo'] . '&StartDay=' . $StartDay . '">' . $StartDay . '</OPTION>';
+			}
 		}
 		echo "</select>";
 		
