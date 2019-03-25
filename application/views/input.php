5afa165e1bd7f1e5fc31fcc27678ee3e80f7fef9
Baron Desmond <baron@desmond.com>
2019-03-25 15:21:54 -0500

    t

--- application/views/input.php
+++ application/views/input.php
@@ -1,11 +1,17 @@
 <?php
 //input select
-
+	if (!isset($_REQUEST['Screen']))
+	{
+		$_REQUEST['Screen'] = 'Employee';
+	}
 	echo '<input type=hidden name="EmpNo" value="' . $_REQUEST['EmpNo'] . '">';
-	echo 'Add Type<select name="Screen"><OPTION>Job</OPTION><OPTION>Employee</OPTION></SELECT>';
+	echo 'Add Type<select name="Screen"><OPTION>Employee</OPTION><OPTION>Job</OPTION><OPTION>Dispatch</OPTION></SELECT>';
 	echo '<input type=hidden name="Offset" value="' . $_REQUEST['Offset'] . '" >';		
-
-	echo 'Event<select name="event"><OPTION>Traveling</OPTION><OPTION>Working</OPTION></select> Job#<input type=text name="JD">';
+	echo '<input type=hidden name="Screen" value="'  .$_REQUEST['Screen'] . '" >';
+	if (isset(${$_REQUEST['Screen']}))
+	{
+		echo ${$_REQUEST['Screen']};
+	}
 
 
 ?>
