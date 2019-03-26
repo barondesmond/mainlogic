352e3a5a57fc96ae857c5b90fe72e502419ecd3a
Baron Desmond <baron@desmond.com>
2019-03-26 13:59:56 -0500

    t

--- application/views/assigncenter.php
+++ application/views/assigncenter.php
@@ -1,9 +1,24 @@
 <?php 
+if (!isset($_REQUEST['JobGroup']))
+{
+	return false;
+}
 if (isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'ASSIGN')
 {
 	echo '<input type=submit class="buttonmain" name="submit" value="ASSIGN">';
 }
-if (isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'REMOVE')
+elseif (isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'REMOVE')
 {
 	echo '<input type=submit class="buttonmain" name="submit" value="REMOVE">';
+}
+else
+{
+	if (isset($employees) && isset($jobgroups) && isset($jobgroupemployees) && count($employees) > 0 && count($jobgroups) > 0 && count($jobgroupemployees)>0)
+	{
+		echo '<input type=submit class="buttonmain" name="submit" value="REMOVE">';
+	}
+	else
+	{
+		echo '<input type=submit class="buttonmain" name="submit" value="ASSIGN">';
+	}
 }
--- application/views/assigncenter2.php.bak
+++ application/views/assigncenter2.php.bak
@@ -3,7 +3,7 @@ if (isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'ASSIGN')
 {
 	echo '<input type=submit class="buttonmain" name="submit" value="SWITCH">';
 }
-elseif (isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'REMOVE' || isset($_REQUEST['JobGroup'])
+elseif (isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'REMOVE' || isset($_REQUEST['JobGroup']))
 {
 	echo '<input type=submit class="buttonmain" name="submit" value="SWITCH">';
 }
