a373f7e5a2cf6491771285be5a25b8742d79ddf1
Baron Desmond <baron@desmond.com>
2019-03-26 13:14:44 -0500

    t

--- application/views/assignperiod.php
+++ application/views/assignperiod.php
@@ -2,7 +2,7 @@ JobGroup <select name=JobGroup onchange="javascript:location.href = this.value;"
 <?php
 foreach ($jobgroups as $jobgroup)
 {
-	echo "<option value='/assign/?JobGroup[]=" . $jobgroup->JobGroupID . "'" . select_group('JobGroupID', $jobgroup->JobGroupID, $_REQUEST['JobGroup']) . " >$jobgroup->JobGroup</option>\r\n";
+	echo "<option value='?JobGroup[]=" . $jobgroup->JobGroupID . "'" . select_group('JobGroupID', $jobgroup->JobGroupID, $_REQUEST['JobGroup']) . " >$jobgroup->JobGroup</option>\r\n";
 }
 ?>
 </select>
