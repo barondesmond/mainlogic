a56bf14abc6c347d24e2c9a45f8b485fb3c40373
Baron Desmond <baron@desmond.com>
2019-03-26 13:47:16 -0500

    t

--- application/views/assigncenter2.php
+++ application/views/assigncenter2.php
@@ -1,9 +1,9 @@
 <?php 
-if (isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'ASSIGN')
+if (isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'ASSIGN'))
 {
 	echo '<input type=submit class="buttonmain" name="submit" value="SWITCH">';
 }
-elseif (isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'REMOVE')
+elseif (isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'REMOVE' || isset($_REQUEST['JobGroup'])
 {
 	echo '<input type=submit class="buttonmain" name="submit" value="SWITCH">';
 }
