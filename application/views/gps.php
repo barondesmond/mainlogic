0a953a6bd9dd74b4b25d2c55461d5e5429f2405a
Baron Desmond <baron@desmond.com>
2019-03-28 14:20:17 -0500

    t

--- application/views/gps.php
+++ application/views/gps.php
@@ -75,7 +75,7 @@ function table_form($row)
 		$id = 0;
 	}
 	
-	$table = '<td><input type=hidden name=file[' . $id . '] value="' . $row->file . '"></td>';
+	$table = '<td><input type=hidden name=file[' . $id . '] value="' . $row->file . '" width="30%"></td>';
 	$id++;
 return $table;
 }
