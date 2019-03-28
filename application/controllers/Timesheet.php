765ee69407135cf3cb9583f20c6bd5edffdc2ca4
Baron Desmond <baron@desmond.com>
2019-03-28 08:53:32 -0500

    t

--- application/controllers/Timesheet.php
+++ application/controllers/Timesheet.php
@@ -161,7 +161,7 @@ class Timesheet extends CI_Controller {
 
 	public function navigation_timesheet()
 	{
-		$this->periodnav = $this->load->view('period', $this, true);
+		$this->periodnav = $this->load->view('period', $this->TimeSheet, true);
 		$this->centercolumn1 = $this->load->view('centercolumn1', $this, true);
 		$this->centercolumn2 = $this->load->view('centercolumn2', $this, true);
 		//$this->inputnav = $this->load->view('input', $this, true);
