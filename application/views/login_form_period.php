f663265eb2bbf585ca1549672118e3851a62faa2
Baron Desmond <baron@desmond.com>
2019-04-01 08:23:33 -0500

    t

--- application/views/login_form_period.php
+++ application/views/login_form_period.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
-Username <input type=text name=username> 
-Password <input type=text name=password>
+Username <input type=text name=username autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" > 
+Password <input type=password name=password>
 
 
