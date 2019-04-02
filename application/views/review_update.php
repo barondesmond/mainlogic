d317b6f08f78384de8e821f4020aa54788b9a626
Baron Desmond <baron@desmond.com>
2019-04-02 10:50:48 -0500

    t

--- application/views/review_update.php
+++ application/views/review_update.php
@@ -18,24 +18,6 @@ if (isset($this->timeclock_review))
 	}
 }
 
-if (isset($_REQUEST['EmpNo']) && $_REQUEST['EmpNo'] != '' && isset($Job[$_REQUEST['EmpNo']]))
-{
-	$db = timeclock_employee($TimeClock);
-	$Job = $db['Job'];
-	$Save = $db['Save'];
-	echo '<input type=hidden name="Offset" value="' . $_REQUEST['Offset'] . '">';		
 
-	echo '<input type=hidden name="EmpNo" value="' . $_REQUEST['EmpNo'] . '">';
-		foreach ($Job[$_REQUEST['EmpNo']] as $screen=>$jd)
-		{
-			foreach ($jd as $key=>$JobDisp)
-			{
-				echo '<p><b>' . $screen . '</b> ' . $JobDisp .  "</p>";
-				echo 'Adjusted<br>';
-				echo $Save[$_REQUEST['EmpNo']][$screen][$key];
-				echo '<br>';
-			}
-		}
-}
 
 ?>
