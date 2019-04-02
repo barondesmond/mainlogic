f11db618072bb594a7f8d5849a1f7854d06d4eab
Baron Desmond <baron@desmond.com>
2019-04-02 09:39:46 -0500

    t

--- application/views/save.php
+++ application/views/save.php
@@ -3,7 +3,7 @@
 
 
 
-if (isset($_REQUEST['EmpNo']) && $_REQUEST['EmpNo'] != '' && isset($Job[$_REQUEST['EmpNo']))
+if (isset($_REQUEST['EmpNo']) && $_REQUEST['EmpNo'] != '' && isset($Job[$_REQUEST['EmpNo']]))
 {
 	$db = timeclock_employee($TimeClock);
 	$Job = $db['Job'];
