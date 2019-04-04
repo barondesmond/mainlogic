2ea13c519b09c92ad54aedc385a87e622b832e81
Baron Desmond <baron@desmond.com>
2019-04-04 15:50:53 -0500

    t

--- application/helpers/authentication_helper.php
+++ application/helpers/authentication_helper.php
@@ -205,9 +205,9 @@ function select_group($key, $id, $JobGroup)
 			$row .= '<font color="red">';
 		}
 		$row .= '<input type=hidden name="TimeClockID' . '[' . $event->TimeClockID . ']' . '[StartDay]" value="' . $event->StartDay . '"><b>' . $event->StartDay . '</b>';
-		$row .= ' Start: <input type=Text name="TimeClockID' . '[' . $event->TimeClockID . ']' . '[StartHour]" value="' . $event->StartHour . '" size="6" maxlength="6" $input>';
+		$row .= ' Start: <input type=Text name="TimeClockID' . '[' . $event->TimeClockID . ']' . '[StartHour]" value="' . $event->StartHour . '" size="8" maxlength="8" $input>';
 		$row .= '<input type=hidden name="TimeClockID' . '[' . $event->TimeClockID . ']' . '[StopDay]" value="' . $event->StopDay . '">';
-		$row .= ' Stop: <input type=text name="TimeClockID' . '[' . $event->TimeClockID . ']' . '[StopHour]" value="' . $event->StopHour . '" size="6" maxlength="6" $input>';
+		$row .= ' Stop: <input type=text name="TimeClockID' . '[' . $event->TimeClockID . ']' . '[StopHour]" value="' . $event->StopHour . '" size="8" maxlength="8" $input>';
 		$row .= ' Event: ' . $event->event ;
 		if (isset($event->updated))
 		{
