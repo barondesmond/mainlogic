e9f40f0ec27c1f00b4bacc3754eaaa5a92093c4e
Baron Desmond <baron@desmond.com>
2019-04-04 16:28:19 -0500

    t

--- application/views/hist.php
+++ application/views/hist.php
@@ -5,7 +5,7 @@
 
 if (isset($_REQUEST['EmpNo']) && $_REQUEST['EmpNo'] != '' && isset($TimeClockHist))
 {
-	$db = timeclock_employee($TimeClockHist);
+	$db = timeclock_employee($TimeClockHist, 'readyonly');
 	$Job = $db['Job'];
 	$Time = $db['Time'];
 	$Chron = $db['Chron'];
