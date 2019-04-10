b06cfd22df734374a5b809bdcac5553289a23cd3
Baron Desmond <baron@desmond.com>
2019-04-10 16:02:57 -0500

    t

--- application/helpers/authentication_helper.php
+++ application/helpers/authentication_helper.php
@@ -392,8 +392,8 @@ function period_select($action='')
 		$date = date("Y-m-d", $event->StartTime);
 		$event->Hours = round(($event->StopTime - $event->StartTime) / (60*60), 2);
 		$event->Date = date("M d Y", $event->StartTime) . ' 12:00:00:00 AM';
-		if ($event->Screen != 'Dispatch')
-		{
+		//if ($event->Screen != 'Dispatch')
+		//{
 			if (!isset($Time[$event->EmpNo][$key][$date]))
 			{
 				$Time[$event->EmpNo][$key][$date] = $event;
@@ -405,7 +405,7 @@ function period_select($action='')
 				$event->Hours = $event->Hours + $rec->Hours;
 				$Time[$event->EmpNo][$key][$date] = $event;
 			}
-		}
+		//}
 
 		if (isset($_REQUEST['EmpNo']) && $_REQUEST['EmpNo'] == $event->EmpNo)
 		{
