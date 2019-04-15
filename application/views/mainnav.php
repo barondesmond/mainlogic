bc28a50aa809ee5f4f68214a6fd182dfc891c935
Baron Desmond <baron@desmond.com>
2019-04-15 15:18:31 -0500

    t

--- application/views/mainnav.php
+++ application/views/mainnav.php
@@ -22,7 +22,7 @@
      </div>
      <div class="clearfix colelem" id="pu4063"><!-- group -->
       <div class="clearfix grpelem" id="u4063"><!-- group -->
-	      <a  href="/accounting/" data-visibility="changed" style="visibility:hidden">
+	      <a  href="/accounting/">
        <img class="grpelem" id="u4159-4" alt="Accounting" src="/images/u4159-4.png?crc=163544750" data-image-width="141"/></A><!-- rasterized frame -->
       </div>
       <div class="clearfix grpelem" id="u4066"><!-- group -->
