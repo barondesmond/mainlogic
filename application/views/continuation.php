02a956903f2ac7c0a307127ebd51a584de8b3bb4
barondesmond <baron@desmond.com>
2019-04-24 09:28:46 -0500

    t

--- application/views/continuation.php
+++ application/views/continuation.php
@@ -2,6 +2,7 @@
 table, th, td {
 border: 1px solid black;
 padding: 1px;
+text-align: center;
 }
 </style>
 
@@ -27,10 +28,10 @@ for ($page=1; $page< $pages; $page++)
 <tr id="row8"><td id="a8">A</td><td id="b8">B</td><td id="c8">C</td><td id="d8">D</td><td id="e8">E</td><td id="f8">F</td><td id="gh8" colspan="2">G</td><td id="i8">H</td><td id="j8">I</td><td id="k8"></td><td id="l8">J</td></tr>
 <tr id="row9"><td id="a9">Item</td><td id="b9">Description of Work</td><td id="c9">Scheduled</td><td id="de9" colspan="2">Work Completed</td><td id="f9">Materials</td><td id="g9" >Total</td><td id="h9">%<td id="i9">Balance</td><td id="j9">Retainage</td><td id="k9"></td><td id="l9"></td></tr>
 <tr id="row10"><td id="a10">No.</td><td id="b10"></td><td id="c10">Value</td><td id="d10">From Previous</td><td id="e10">This Period</td><td id="f10">Presently</td><td id="g10" >Completed</td><td id="h10">(G/C)<td id="i10">To Finish</td><td id="j1-">(If Variable</td><td id="k10"></td><td id="l10">Item</td></tr>
-<tr id="row11"><td id="a11"></td><td id="b11"></td><td id="c11"></td><td id="d11">Application</td><td id="e11"></td><td id="f11">Stored</td><td id="g11" >And Stored</td><td id="h11"><td id="i11">)C - G)</td><td id="j11">Rate)</td><td id="k11"></td><td id="l11">Variable</td></tr>
+<tr id="row11"><td id="a11"></td><td id="b11"></td><td id="c11"></td><td id="d11">Application</td><td id="e11"></td><td id="f11">Stored</td><td id="g11" >And Stored</td><td id="h11"><td id="i11">(C - G)</td><td id="j11">Rate)</td><td id="k11"></td><td id="l11">Variable</td></tr>
 <tr id="row12"><td id="a12"></td><td id="b12"></td><td id="c12"></td><td id="d12">(D + E)</td><td id="e12"></td><td id="f12">(Not In</td><td id="g12" >To Date</td><td id="h12"><td id="i12"></td><td id="j12"></td><td id="k12"></td><td id="l12">Retainage</td></tr>
 <tr id="row13"><td id="a13"></td><td id="b13"></td><td id="c13"></td><td id="d13"></td><td id="e13"></td><td id="f13">D or E)</td><td id="g13" >(D + E + F)</td><td id="h1e"><td id="i12"></td><td id="j12"></td><td id="k12"></td><td id="l12">Rate (%)</td></tr>';
-$collet = array('a' => '{row}', 'b' => '<input type=text name="continuation[{page}][{row}][{col}]" value={val}>', 'c' => '<input type=text name="continuation[{page}][{row}][{col}]" value="{val}">' , 'd' => '{val}', 'e' => '<input type=text name="continuation[{page}][{row}][{col}]" value={val}>', 'f' => '<input type=text name="continuation[{page}][{row}][{col}]" value={val}>', 'g' => '{val}', 'h' => '{val}', 'i' => '{val}', 'j' => '{val}', 'k' => '{val}', 'l' => '<input type=text name="continuation[{page}][{row}][{col}]" value="{val}">');
+$collet = array('a' => '{row}', 'b' => '<input type=text name="continuation[{page}][{row}][{col}]" value="{val}"  size="30">', 'c' => '<input type=text name="continuation[{page}][{row}][{col}]" value="{val}" maxlength="10" size="10">' , 'd' => '<input type=text name="continuation[{page}][{row}][{col}]" value="{val}" maxlength="10" size="10">', 'e' => '<input type=text name="continuation[{page}][{row}][{col}]" value="{val}" maxlength="10" size="10">', 'f' => '<input type=text name="continuation[{page}][{row}][{col}]" value="{val}" maxlength="10" size="10">', 'g' => '{val}', 'h' => '{val}', 'i' => '{val}', 'j' => '{val}', 'k' => '{val}', 'l' => '<input type=text name="continuation[{page}][{row}][{col}]" value="{val}" maxlength="5" size="5">');
 
 for ($row=1; $row < $rows; $row++)
 {
